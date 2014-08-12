c82ae26fe9d3d463f4fd404bc42652b2507a7168
Jamie Piazza <piazza.jamie@gmail.com>
2014-08-11 18:47:58 -0700

    add conditional alert
    second alert depending on total value

--- LifetimeSupplyCalc/script.js
+++ LifetimeSupplyCalc/script.js
@@ -5,3 +5,10 @@ var total = ((maxAge - age) * glassesPerDay) * 365;
 var answer = "You will need " + total + " glasses of water to last you until your old age of " + maxAge + ".";
 
 alert(answer);
+
+if (total > 40000) {
+	alert("Wow! That's a lot!");
+}
+ 	else {
+ 		alert("You seem pretty reasonable!");
+ 	}
