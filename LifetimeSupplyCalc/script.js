c8d47b25622bad8ff6a949fd9cb6acd46f9e1b2a
Jamie Piazza <piazza.jamie@gmail.com>
2014-08-11 18:46:51 -0700

    index.html and script.js calculate total and alert
    index.html and script.js setup for Lifetime Supply Calc w/ total alert
    msg

---
+++ LifetimeSupplyCalc/script.js
@@ -0,0 +1,7 @@
+var age = 35;
+var maxAge = 97;
+var glassesPerDay = 8;
+var total = ((maxAge - age) * glassesPerDay) * 365;
+var answer = "You will need " + total + " glasses of water to last you until your old age of " + maxAge + ".";
+
+alert(answer);
