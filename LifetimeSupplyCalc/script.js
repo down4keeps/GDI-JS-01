bd9c7b426fec355edec12559843ed4bb5b09f553
Jamie Piazza <piazza.jamie@gmail.com>
2014-08-12 13:45:06 -0700

    button / calculate() function
    add button to index.html that runs new calculate() function in script.js

--- LifetimeSupplyCalc/script.js
+++ LifetimeSupplyCalc/script.js
@@ -1,14 +1,18 @@
 var age = 35;
 var maxAge = 97;
 var glassesPerDay = 8;
-var total = ((maxAge - age) * glassesPerDay) * 365;
-var answer = "You will need " + total + " glasses of water to last you until your old age of " + maxAge + ".";
 
-alert(answer);
+function calculate() {
+	var total = ((maxAge - age) * glassesPerDay) * 365;
+	var answer = "You will need " + total + " glasses of water to last you until your old age of " + maxAge + ".";
 
-if (total > 40000) {
-	alert("Wow! That's a lot!");
-}
- 	else {
- 		alert("You seem pretty reasonable!");
- 	}
+	alert(answer);
+
+	if (total > 40000) {
+		alert("Wow! That's a lot!");
+	}
+	 	else {
+	 		alert("You seem pretty reasonable!");
+	 	}
+
+	}
